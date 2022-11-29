8fa7af3c72d7871b20e020500a96de63acc33c9a
raxonpc <tomeczekorlowski1@gmail.com>
2022-11-29 02:49:12 +0100

    Set rate limiter

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-import express from "express"
-import { MongoClient, ServerApiVersion } from "mongodb"
-import dotenv from "dotenv"
+import express from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import dotenv from 'dotenv'
+import rateLimit from 'express-rate-limit'
 
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT ?? 8080;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 5, // Limit each IP to 1 requests per `window` (here, per 10 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(express.static('pages', { root: '.' }));
 app.use(express.json());
@@ -17,7 +26,7 @@ app.get("/", (req, res) => {
     res.sendFile('index.html');
 });
 
-app.post("/", async (req, res) => {
+app.post("/", limiter, async (req, res) => {
     const email = req.body.email;
 
     try {
